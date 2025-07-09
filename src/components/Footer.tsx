da6ad52d8559f85c9a1001b56ccbee58ce926279
LonathG <lk.gcm29@gmail.com>
2025-07-09 23:34:07 +0530

    Footer color fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,6 @@ const Footer: React.FC = () => {
   return (
     <div
       className="w-screen h-fit z-10 relative font-AlbertSans_Regular bg-[#4169E1] text-white"
-      style={{
-        background: `linear-gradient(to bottom, #4169E1 0%, #6A8FE8 20%, #2C3E99 100%)`,
-      }}
     >
       <div className="w-full h-fit bg-green lg:px-24 opacity-80 flex justify-between items-center text-sm font-extralight text-text px-4 md:px-8 relative -bottom-8">
         <div>Sample Project By WebMinds Designs</div>

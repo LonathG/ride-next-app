c103040a7e0854da4868f5ac2091fa77c1e2e7b0
LonathG <lk.gcm29@gmail.com>
2025-07-09 20:02:26 +0530

    Footer updated with new components and styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { FizziLogo } from "./FizziLogo";
+import TextPressure from './TextPressure';
 
 type Props = {};
 
 export default function Footer({}: Props) {
-  return (
-    <footer className="bg-[#FEE832] text-[#FE6334]">
+  return (<>
+    <footer className="bg-[#346ec9] text-[#e1e6ed]">
       <div className="relative mx-auto flex w-full max-w-4xl justify-center px-4 py-10">
         <FizziLogo />
         <div className="absolute right-24 top-0 size-28 origin-center -translate-y-14 md:size-48 md:-translate-y-28">
         </div>
       </div>
     </footer>
+    </>
   );
 }
--- src/slices/BigText/index.tsx
+++ src/slices/BigText/index.tsx
@@ -4,26 +4,7 @@ import { SliceComponentProps } from "@prismicio/react";
 export type BigTextProps = SliceComponentProps<Content.BigTextSlice>;
 
 const BigText = ({ slice }: BigTextProps): JSX.Element => {
-  return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-      className="min-h-screen w-screen overflow-hidden bg-[#FE6334] text-[#FEE832] px-4" // added horizontal padding
-    >
-      <h2 className="grid w-full gap-[3vw] py-10 text-center font-black uppercase leading-[0.7]">
-        {/* RIDE large text */}
-        <div className="text-[34vw]">RIDE</div>
-        
-        {/* YOUR ENERGY split for responsive layout */}
-        <div className="grid gap-[3vw] text-[44vw] md:flex md:text-[11vw]">
-          <span className="inline-block">YOUR </span>
-          <span className="inline-block max-md:text-[27vw]">ENERGY </span>
-        </div>
-
-        {/* HIGH large text */}
-        <div className="text-[32vw]">HIGH</div>
-      </h2>
-    </section>
+  return (<></>
   );
 };
 
